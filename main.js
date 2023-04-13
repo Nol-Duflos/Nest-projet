a02429068d254464509b121a89952d4c60b71c61
Nolwenn <duflos.nol@laposte.net>
2023-04-13 15:08:17 +0200

    Ajout du main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navigationNavbar = document.querySelector(".navigation-navbar");
+const btnNavbar = document.querySelector(".btn-navbar");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navigationNavbar.classList.toggle("active");
+    btnNavbar.classList.toggle("active");
+}
+
+//Dans la media Query 
