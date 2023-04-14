3ee99a8f03ef946c6e24c3f25061e6e3caa0b1ac
Nolwenn <duflos.nol@laposte.net>
2023-04-14 12:41:07 +0200

    commit midi, mon JS ne marche toujours pas

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-document.querySelector(".navbar .hamburger").addEventListener("click",function(){
+//Menu Hamburger responsive
+
+document.querySelector(".navbar .hamburger-btn").addEventListener("click", ()=>{
     document.querySelector(".navbar").classList.toggle("active");
 });
 
