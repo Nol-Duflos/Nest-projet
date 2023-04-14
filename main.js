515efa761e0c2665cd2327b065ed744cdbafe7f3
Nolwenn <duflos.nol@laposte.net>
2023-04-14 14:27:27 +0200

    Le menu burger ne fonctionne toujours pas

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-//Menu Hamburger responsive
-
-document.querySelector(".navbar .hamburger-btn").addEventListener("click", ()=>{
-    document.querySelector(".navbar").classList.toggle("active");
-});
-
-
-
-
 //Année du copyright
 
 const year = document.querySelector('#current-year');
 
 year.innerHTML = new Date().getFullYear();
+
+//Menu Hamburger responsive
+
+document.querySelector(".navbar .hamburger-btn").addEventListener("click", ()=>{
+    document.querySelector(".navbar").classList.toggle("active");
+});
